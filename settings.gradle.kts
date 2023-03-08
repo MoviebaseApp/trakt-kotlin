e1d3e52f7c3b1d2c3ca618ba643bba20dcb46df5
Christian Krueger <15275971+chrisnkrueger@users.noreply.github.com>
2023-03-08 16:10:16 +0100

    Change to Gradle catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,19 @@ pluginManagement {
         mavenCentral()
     }
 }
+
+plugins {
+    id("com.gradle.enterprise") version "3.12.4"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishAlways()
+    }
+}
+
 rootProject.name = "trakt-api"
 
 include(":lib")
