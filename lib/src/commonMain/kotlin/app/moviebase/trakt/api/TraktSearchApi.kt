607163c619b580d08e2659c37ecfbea0c3460488
Christian Krueger <15275971+chrisnkrueger@users.noreply.github.com>
2023-03-09 09:53:41 +0100

    Switch to default requests

--- lib/src/commonMain/kotlin/app/moviebase/trakt/api/TraktSearchApi.kt
+++ lib/src/commonMain/kotlin/app/moviebase/trakt/api/TraktSearchApi.kt
@@ -1,13 +1,10 @@
 package app.moviebase.trakt.api
 
-import app.moviebase.trakt.core.endPoint
+import app.moviebase.trakt.core.getByPaths
 import app.moviebase.trakt.model.TraktMediaType
 import app.moviebase.trakt.model.TraktSearchResult
 import app.moviebase.trakt.model.TraktSearchType
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.HttpRequestBuilder
-import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 
 class TraktSearchApi(private val client: HttpClient) {
@@ -19,12 +16,13 @@ class TraktSearchApi(private val client: HttpClient) {
      *
      * @see [Search - ID Lookup](https://trakt.docs.apiary.io/#reference/search/id-lookup)
      */
-    suspend fun searchIdLookup(searchType: TraktSearchType, id: String, mediaType: TraktMediaType): List<TraktSearchResult> = client.get {
-        endPointSearch(searchType.value, id)
+    suspend fun searchIdLookup(
+        searchType: TraktSearchType,
+        id: String,
+        mediaType: TraktMediaType,
+    ): List<TraktSearchResult> = client.getByPaths(*pathSearch(searchType.value, id)) {
         parameter("type", mediaType.value)
-    }.body()
-
-    private fun HttpRequestBuilder.endPointSearch(vararg paths: String) {
-        endPoint("search", *paths)
     }
+
+    private fun pathSearch(traktSlug: String, vararg paths: String) = arrayOf("search", traktSlug, *paths)
 }
