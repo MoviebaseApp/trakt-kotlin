8db05d4ece0c65421cbfccc703f2eefc7d776f6a
Christian Krueger <15275971+chrisnkrueger@users.noreply.github.com>
2023-03-09 09:54:43 +0100

    Switch to Coroutine tests

--- lib/src/jvmTest/kotlin/app/moviebase/trakt/api/TraktSearchApiTest.kt
+++ lib/src/jvmTest/kotlin/app/moviebase/trakt/api/TraktSearchApiTest.kt
@@ -4,7 +4,7 @@ import app.moviebase.trakt.core.mockHttpClient
 import app.moviebase.trakt.model.TraktMediaType
 import app.moviebase.trakt.model.TraktSearchType
 import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 
 class TraktSearchApiTest {
@@ -18,7 +18,7 @@ class TraktSearchApiTest {
     val classToTest = TraktSearchApi(client)
 
     @Test
-    fun `it can search tmdb show`() = runBlocking {
+    fun `it can search tmdb show`() = runTest {
         val results = classToTest.searchIdLookup(TraktSearchType.TMDB, "63639", TraktMediaType.SHOW)
 
         val showResult = results.first()
--- lib/src/jvmTest/kotlin/app/moviebase/trakt/api/TraktShowsApiTest.kt
+++ lib/src/jvmTest/kotlin/app/moviebase/trakt/api/TraktShowsApiTest.kt
@@ -2,7 +2,7 @@ package app.moviebase.trakt.api
 
 import app.moviebase.trakt.core.mockHttpClient
 import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 
 class TraktShowsApiTest {
@@ -16,7 +16,7 @@ class TraktShowsApiTest {
     val classToTest = TraktShowsApi(client)
 
     @Test
-    fun `it can fetch show summary`() = runBlocking {
+    fun `it can fetch show summary`() = runTest {
         val traktShow = classToTest.getSummary("vikings")
 
         assertThat(traktShow.title).isEqualTo("Vikings")
