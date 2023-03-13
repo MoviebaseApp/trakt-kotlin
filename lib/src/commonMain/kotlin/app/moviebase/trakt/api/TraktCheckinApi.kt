99c9e2923c38975b761e0fcab5b0fef41498a678
Chris Banes <chris@banes.me>
2023-03-13 12:56:16 +0000

    Fix POST operations
    We need to set the content type, so that Ktor can
    serialize the body appropriately.

--- lib/src/commonMain/kotlin/app/moviebase/trakt/api/TraktCheckinApi.kt
+++ lib/src/commonMain/kotlin/app/moviebase/trakt/api/TraktCheckinApi.kt
@@ -6,10 +6,13 @@ import app.moviebase.trakt.model.TraktCheckin
 import app.moviebase.trakt.model.TraktCheckinItem
 import io.ktor.client.HttpClient
 import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 
 class TraktCheckinApi(private val client: HttpClient) {
 
     suspend fun postCheckin(item: TraktCheckinItem): TraktCheckin.Active = client.postByPaths("checkin") {
+        contentType(ContentType.Application.Json)
         setBody(item)
     }
 
