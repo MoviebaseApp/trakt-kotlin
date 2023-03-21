953546553f835acced4ef5fcc4ea9bc9cfdd6293
Christian Krueger <15275971+chrisnkrueger@users.noreply.github.com>
2023-03-21 15:51:44 +0100

    Add comments endpoint

---
+++ lib/src/commonMain/kotlin/app/moviebase/trakt/api/TraktCommentsApi.kt
@@ -0,0 +1,40 @@
+package app.moviebase.trakt.api
+
+import app.moviebase.trakt.core.deleteByPaths
+import app.moviebase.trakt.core.getByPaths
+import app.moviebase.trakt.core.postByPaths
+import app.moviebase.trakt.core.putByPaths
+import app.moviebase.trakt.model.TraktPostComment
+import app.moviebase.trakt.model.TraktCheckin
+import app.moviebase.trakt.model.TraktComment
+import io.ktor.client.HttpClient
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+
+class TraktCommentsApi(private val client: HttpClient) {
+
+    suspend fun postComment(comment: TraktPostComment): TraktCheckin.Active = client.postByPaths("comments") {
+        contentType(ContentType.Application.Json)
+        setBody(comment)
+    }
+
+    suspend fun updateComment(id: Int, comment: TraktPostComment): TraktComment = client.putByPaths("comments", id.toString()) {
+        contentType(ContentType.Application.Json)
+        setBody(comment)
+    }
+
+    suspend fun getComment(id: Int): TraktComment = client.getByPaths("comments", id.toString())
+
+    suspend fun deleteComment(id: Int): TraktComment = client.deleteByPaths("comments", id.toString())
+
+    suspend fun getCommentReplies(id: Int): List<TraktComment> = client.getByPaths("comments", id.toString(), "replies")
+
+    suspend fun postCommentReplies(
+        id: Int,
+        comment: TraktPostComment,
+    ): List<TraktComment> = client.postByPaths("comments", id.toString(), "replies") {
+        contentType(ContentType.Application.Json)
+        setBody(comment)
+    }
+}
